29894aa9b45d9a9ff2681935de9623fc86148cd2
Jordan Golden <karrmaazan@gmail.com>
2026-08-08 16:15:30 -0700

    Update archive landing image styling

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -25,7 +25,7 @@ const TopLink = styled(NavLink)`
   top: 2rem;
   right: calc(1rem + 2vw);
   text-decoration: none;
-  z-index: 2;
+  z-index: 3;
 `;
 
 const RightLink = styled(NavLink)`
@@ -35,17 +35,18 @@ const RightLink = styled(NavLink)`
   right: calc(1rem + 2vw);
   transform: rotate(90deg) translate(-50%, -50%);
   text-decoration: none;
-  z-index: 2;
+  z-index: 3;
 `;
 
 const LeftLink = styled(NavLink)`
-  color: ${(props) => (props.$click ? props.theme.body : props.theme.text)};
+  color: ${(props) =>
+    props.$click ? props.theme.body : props.theme.text};
   position: absolute;
   top: 48%;
   left: calc(1rem + 2vw);
   transform: translate(-50%, -50%) rotate(-90deg);
   text-decoration: none;
-  z-index: 2;
+  z-index: 3;
 `;
 
 const BottomBar = styled.div`
@@ -56,6 +57,7 @@ const BottomBar = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-evenly;
+  z-index: 3;
 `;
 
 const BottomLink = styled(NavLink)`
@@ -64,13 +66,19 @@ const BottomLink = styled(NavLink)`
     if (props.$darkOnClick) return "#000";
     return props.theme.body;
   }};
+
   text-decoration: none;
-  z-index: 2;
+  z-index: 3;
 `;
 
 const rotate = keyframes`
-  from { transform: rotate(0); }
-  to { transform: rotate(360deg); }
+  from {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
 const Center = styled.button`
@@ -78,6 +86,7 @@ const Center = styled.button`
   top: ${(props) => (props.$click ? "85%" : "50%")};
   left: ${(props) => (props.$click ? "92%" : "50%")};
   transform: translate(-50%, -50%);
+
   border: none;
   outline: none;
   background-color: transparent;
@@ -87,14 +96,17 @@ const Center = styled.button`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   transition: all 1s ease;
+  z-index: 4;
 
   & > :first-child {
     animation: ${rotate} infinite 1.5s linear;
   }
 
   & > :last-child {
-    display: ${(props) => (props.$click ? "none" : "inline-block")};
+    display: ${(props) =>
+      props.$click ? "none" : "inline-block"};
     padding-top: 1rem;
   }
 `;
@@ -102,13 +114,68 @@ const Center = styled.button`
 const DarkDiv = styled.div`
   position: absolute;
   top: 0;
-  background-color: #000;
   bottom: 0;
   right: 50%;
-  width: ${(props) => (props.$click ? "50%" : "0%")};
-  height: ${(props) => (props.$click ? "100%" : "0%")};
+
+  background-color: #000;
+
+  width: ${(props) =>
+    props.$click ? "50%" : "0%"};
+
+  height: ${(props) =>
+    props.$click ? "100%" : "0%"};
+
   z-index: 1;
-  transition: height 0.5s ease, width 1s ease 0.5s;
+
+  transition:
+    height 0.5s ease,
+    width 1s ease 0.5s;
+`;
+
+/*
+  CENTER ARCHIVE IMAGE
+
+  Current image:
+  public/TSD.PNG
+
+  You can make it smaller or bigger by changing:
+  width: min(38vw, 480px);
+*/
+const ArchiveImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+width: min(75vw, 950px);
+  height: auto;
+  max-height: 78vh;
+
+  object-fit: contain;
+
+  opacity: ${(props) =>
+    props.$click ? 1 : 0};
+
+  transform: ${(props) =>
+    props.$click
+      ? "translate(-50%, -50%) scale(1)"
+      : "translate(-50%, -50%) scale(0.9)"};
+
+  filter:
+    drop-shadow(1px 0 0 white)
+    drop-shadow(-1px 0 0 white)
+    drop-shadow(0 1px 0 white)
+    drop-shadow(0 -1px 0 white)
+    drop-shadow(1px 1px 0 white)
+    drop-shadow(-1px 1px 0 white)
+    drop-shadow(1px -1px 0 white)
+    drop-shadow(-1px -1px 0 white);
+
+  transition:
+    opacity 1s ease 0.7s,
+    transform 1.4s ease 0.7s;
+
+  z-index: 2;
+  pointer-events: none;
 `;
 
 export default function BrandLandingPage() {
@@ -116,60 +183,121 @@ export default function BrandLandingPage() {
 
   return (
     <MainContainer>
+      {/* Half-and-half background */}
       <DarkDiv $click={click} />
+
+      {/* Center image on top of the split */}
+      <ArchiveImage
+        src="/TSD.PNG"
+        alt="The Soul Dweller"
+        $click={click}
+      />
+
       <Container>
         <PowerButton />
-        <LogoComponent theme={click ? "dark" : "light"} />
-        <SocialIcons theme={click ? "dark" : "light"} />
 
-        <Center $click={click} onClick={() => setClick(!click)}>
-          <YinYang width={click ? 120 : 200} height={click ? 120 : 200} fill="currentColor" />
+        <LogoComponent
+          theme={click ? "dark" : "light"}
+        />
+
+        <SocialIcons
+          theme={click ? "dark" : "light"}
+        />
+
+        <Center
+          $click={click}
+          onClick={() => setClick(!click)}
+        >
+          <YinYang
+            width={click ? 120 : 200}
+            height={click ? 120 : 200}
+            fill="currentColor"
+          />
+
           <span>enter archive</span>
         </Center>
 
         <TopLink to="/overview">
-          <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
+          <motion.h2
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.96 }}
+          >
             Overview
           </motion.h2>
         </TopLink>
 
         <RightLink to="/brand">
-          <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
+          <motion.h2
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.96 }}
+          >
             Brand
           </motion.h2>
         </RightLink>
 
-        <LeftLink to="/ips" $click={click}>
-          <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
+        <LeftLink
+          to="/ips"
+          $click={click}
+        >
+          <motion.h2
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.96 }}
+          >
             IP Library
           </motion.h2>
         </LeftLink>
 
-<BottomBar>
-  <BottomLink to="/characters" $click={click}>
-    <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
-      Characters
-    </motion.h2>
-  </BottomLink>
+        <BottomBar>
+          <BottomLink
+            to="/characters"
+            $click={click}
+          >
+            <motion.h2
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.96 }}
+            >
+              Characters
+            </motion.h2>
+          </BottomLink>
 
-  <BottomLink to="/lore" $click={click}>
-    <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
-      Lore
-    </motion.h2>
-  </BottomLink>
+          <BottomLink
+            to="/lore"
+            $click={click}
+          >
+            <motion.h2
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.96 }}
+            >
+              Lore
+            </motion.h2>
+          </BottomLink>
 
-  <BottomLink to="/projects" $click={click} $darkOnClick>
-    <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
-      Projects
-    </motion.h2>
-  </BottomLink>
+          <BottomLink
+            to="/projects"
+            $click={click}
+            $darkOnClick
+          >
+            <motion.h2
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.96 }}
+            >
+              Projects
+            </motion.h2>
+          </BottomLink>
 
-  <BottomLink to="/assets" $click={click} $darkOnClick>
-    <motion.h2 whileHover={{ scale: 1.08 }} whileTap={{ scale: 0.96 }}>
-      Assets
-    </motion.h2>
-  </BottomLink>
-</BottomBar>
+          <BottomLink
+            to="/assets"
+            $click={click}
+            $darkOnClick
+          >
+            <motion.h2
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.96 }}
+            >
+              Assets
+            </motion.h2>
+          </BottomLink>
+        </BottomBar>
       </Container>
     </MainContainer>
   );
